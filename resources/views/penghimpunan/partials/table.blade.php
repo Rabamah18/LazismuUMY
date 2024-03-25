fae577687fe06bda8ccbbbeb029f861e0a609541
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2024-03-25 22:08:42 +0700

    fix table and pagination

--- resources/views/penghimpunan/partials/table.blade.php
+++ resources/views/penghimpunan/partials/table.blade.php
@@ -1,4 +1,4 @@
-<div class="relative mt-6 overflow-x-visible overflow-y-visible rounded-md md:block">
+<div class="relative mt-6 overflow-x-scroll overflow-y-visible rounded-md md:block">
     <table class="w-full text-base text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
             <tr>
