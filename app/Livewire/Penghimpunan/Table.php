ed819509c1450e2ffd87abd34a9dae7edc1d140b
HabibSantoso <habib.santosoti18@gmail.com>
2024-10-11 21:11:16 +0700

    livewire search and filter

--- app/Livewire/Penghimpunan/Table.php
+++ app/Livewire/Penghimpunan/Table.php
@@ -15,21 +15,47 @@ class Table extends Component
 
     public $sumberDanas;
 
+    public $selectedSumberDana;
+
     public $programSumbers;
+
+    public $selectedProgramSumber;
 
     public $tahuns;
 
+    public $selectedTahun;
+
+    public $search;
+
+    public $paginate = 30;
+
     public function mount()
     {
-        $this->sumberDanas = SumberDana::all();
-        $this->programSumbers = ProgramSumber::all();
-        $this->tahuns = Tahun::all();
+        $this->sumberDanas = SumberDana::query()->get();
+        $this->programSumbers = ProgramSumber::query()->get();
+        $this->tahuns = Tahun::query()->get();
 
     }
 
     public function render()
     {
-        $penghimpunans = Penghimpunan::orderByDesc('updated_at')->paginate(10);
+        $penghimpunans = Penghimpunan::orderByDesc('updated_at')
+            ->when($this->search, function ($query): void {
+                $query->where(function ($query) {
+                    $query->where('uraian', 'like', '%'.$this->search.'%')
+                        ->orWhere('tanggal', 'like', '%'.$this->search.'%');
+                });
+            })
+            ->when($this->selectedSumberDana, function ($query) {
+                $query->where('sumber_dana_id', $this->selectedSumberDana);
+            })
+            ->when($this->selectedProgramSumber, function ($query) {
+                $query->where('program_sumber_id', $this->selectedProgramSumber);
+            })
+            ->when($this->selectedTahun, function ($query) {
+                $query->where('tahun_id', $this->selectedTahun);
+            })
+            ->paginate($this->paginate);
 
         return view('livewire.penghimpunan.table', ['penghimpunans' => $penghimpunans]);
     }
