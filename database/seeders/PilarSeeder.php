a7eec2dd6d9705c510e83a9395f70bfd377ca897
Rabamah18 <bramantyo.adi.ft18@mail.umy.ac.id>
2024-10-22 15:17:08 +0700

    add seeder pilar dan program pilar

--- database/seeders/PilarSeeder.php
+++ database/seeders/PilarSeeder.php
@@ -12,13 +12,59 @@ class PilarSeeder extends Seeder
      */
     public function run(): void
     {
-        Pilar::factory()->count(6)->sequence(
-            ['name' => 'Pendidikan'],
-            ['name' => 'Kesehatan'],
-            ['name' => 'Ekonomi'],
-            ['name' => 'Kemanusiaan'],
-            ['name' => 'Sosial Dakwah'],
-            ['name' => 'Lingkungan']
-        )->create();
+       $pilar = [
+        'Pendidikan' => [
+            'Beasiswa Sang Surya',
+            'Beasiswa Mentari',
+            'Save Our School',
+            'Peduli Guru',
+            'Lazismu Goes To Campus',
+            'Muhammadiyah Scholarship Preparation Program (MSPP)',
+        ],
+
+        'Kesehatan' => [
+            'Peduli Kesehatan',
+            'Indonesia Mobile Clinic',
+            'SAUM',
+            'TIMBANG',
+            'ENDTB',
+            'Rumah Singgah Pasien',
+            'Khitan Gratis',
+        ],
+
+        'Ekonomi' => [
+            'Pemberdayaan UMKM',
+            'Peternakan Masyarakat Mandiri',
+            'Tani Bangkit',
+            'Ketahanan Pangan',
+        ],
+
+        'Kemanusiaan' => [
+            'Indonesia Siaga',
+            'Sekolah Cerdas',
+            'Muhammadiyah Aid',
+        ],
+
+        'Sosial Dakwah' => [
+            'Pemberdayaan Disabilitas',
+            'Sayangi lansia',
+            'Pembedayaan Mualaf',
+            'Back to Masjid',
+            'Qurban',
+        ],
+
+        'Lingkungan' => [
+            'Peduli Lingkungan',
+            'Sayangi Daratmu',
+            'Sayangi Lautmu',
+        ],
+       ];
+
+       foreach($pilar as $pilarName => $programPilar) {
+           $pilar = Pilar::factory()->create(['name' => $pilarName]);
+           foreach ($programPilar as $programPilarName) {
+               $pilar->programPilars()->create(['name' => $programPilarName]);
+           }
+       }
     }
 }
