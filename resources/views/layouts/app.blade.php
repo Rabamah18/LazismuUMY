1501724d85faa884f4f5ea4732b801b0391214eb
HabibSantoso <habib.santosoti18@gmail.com>
2025-02-12 14:38:44 +0700

    mengubah favicon jadi logo my ziska

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <link rel="icon" type="image/x-icon" href="/logo/Logo_MyZiska.png">
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
